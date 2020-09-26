fc40f80e0b30d4ad419a7a812ce5c8d13a37243a
yg <yonggeol93@gmail.com>
2020-09-26 15:11:39 +0900

    Revert "object localize project setting"
    This reverts commit 550820ab37a305d6993c42723153c2669f0b24d3.

--- data/img_download.py
+++ data/img_download.py
@@ -1,18 +1,21 @@
-import argparse
-import multiprocessing
-
-import numpy as np
 import json
 import glob
 import os
 
-import asyncio
-import aiohttp
-from PIL import Image
-from io import BytesIO
+# img_download
+meta_path = 'meta/*.json'
 
-META_PATH = 'meta/train/*.json'
-DST_FOLDER_PATH = 'test/'
+meta_info_paths = glob.glob(meta_path)
+
+img_urls = []
+
+# img url parsing
+for meta_info_path in meta_info_paths:
+    with open(meta_info_path) as json_file:
+        json_data = json.load(json_file)
+
+    img_url = json_data['url']
+    img_urls.append(img_url)
 
 # download img sync
 # 코루틴으로 이미지 다운로드 받기
@@ -20,11 +23,12 @@ DST_FOLDER_PATH = 'test/'
 # await : 병목이 발생해서 다른 작업을 통제권을 넘겨줄 필요가 있는 부분에서는 await을 써줌
 # await 뒤에 오는 함수도 코루틴으로 작성되어야 함.
 
-#입력 파라메타 파싱
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--n_worker', type=int)
-    return parser.parse_args()
+import asyncio
+import aiohttp
+from PIL import Image
+from io import BytesIO
+
+dst_folder_path = 'img'
 
 async def download_imgs(img_urls, dst_folder_path):
 
@@ -52,40 +56,10 @@ async def download_imgs(img_urls, dst_folder_path):
     async with aiohttp.ClientSession() as sess:
         await fetch_all(sess, img_urls, dst_folder_path)
 
-def main(img_urls):
+def main():
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(download_imgs(img_urls, DST_FOLDER_PATH))
+    loop.run_until_complete(download_imgs(img_urls, dst_folder_path))
     loop.close()
 
-
 if __name__ == '__main__':
-
-    # params
-    args = get_args()
-    n_worker = args.n_worker
-
-    meta_info_paths = glob.glob(META_PATH)
-
-    srcs = []
-    # img url parsing
-    for meta_info_path in meta_info_paths:
-        with open(meta_info_path) as json_file:
-            json_data = json.load(json_file)
-
-        img_url = json_data['url']
-        srcs.append(img_url)
-
-    # split arr with n chunks
-    src_chunks = np.array_split(srcs, n_worker)
-
-    # multi-processing
-    pool = multiprocessing.Pool(processes=n_worker)
-    pool.starmap(main, zip(src_chunks,))
-
-    pool.close()
-    pool.join()
-
-    # 7961 img download
-    # 1 : 56초
-    # 5 : 16초
-    # 10 : 10초
+    main()
